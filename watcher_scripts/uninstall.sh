a7bd7cd1f2a128e488702d00fd3ec9d4bc9de896
Chris <schauer@inf-schauer.de>
2023-12-01 22:30:51 +0100

    fix uninstall script

--- watcher_scripts/uninstall.sh
+++ watcher_scripts/uninstall.sh
@@ -2,6 +2,6 @@
 sudo systemctl stop blackout_watch_heartbeat.service
 sudo systemctl stop blackout_watch_server.service
 sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
-sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
+sudo rm /etc/systemd/system/blackout_watch_server.service
 sudo systemctl daemon-reload
 
