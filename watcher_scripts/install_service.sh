20bc667dedcac13e05281d1909d9399fd0991348
Chris <schauer@inf-schauer.de>
2023-12-01 20:37:31 +0100

    fix heartbeat service

--- watcher_scripts/install_service.sh
+++ watcher_scripts/install_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp *.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable blackout_watch_hearbeat.service
+sudo systemctl enable blackout_watch_heartbeat.service
 sudo systemctl enable blackout_watch_server.service
-sudo systemctl start blackout_watch_hearbeat.service
+sudo systemctl start blackout_watch_heartbeat.service
 sudo systemctl start blackout_watch_server.service
