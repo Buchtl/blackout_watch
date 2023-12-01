5098cad0a6ef18e0577110f87049d00298b79587
Chris <schauer@inf-schauer.de>
2023-12-01 22:28:50 +0100

    feat: uninstall script

---
+++ watcher_scripts/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo systemctl stop blackout_watch_heartbeat.service
+sudo systemctl stop blackout_watch_server.service
+sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
+sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
+sudo systemctl daemon-reload
+
