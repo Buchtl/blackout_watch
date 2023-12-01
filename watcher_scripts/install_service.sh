850600a7b1d5b56f73a75c800c11c638c27a8606
Chris <schauer@inf-schauer.de>
2023-12-01 20:33:09 +0100

    refactor and start server

--- watcher_scripts/install_service.sh
+++ watcher_scripts/install_service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo cp blackout_watch.service /etc/systemd/system/
+sudo cp *.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable blackout_watch.service
-sudo systemctl start blackout_watch.service
+sudo systemctl enable blackout_watch_hearbeat.service
+sudo systemctl enable blackout_watch_server.service
+sudo systemctl start blackout_watch_hearbeat.service
+sudo systemctl start blackout_watch_server.service
---
+++ watcher_scripts/start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/pi/blackout_watch/flask/main.py
