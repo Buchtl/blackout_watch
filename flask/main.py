990ec335797b311873f3d2056e888f89bd0e92c6
Chris <schauer@inf-schauer.de>
2023-12-02 14:52:42 +0100

    flask now using downtimes

--- flask/main.py
+++ flask/main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import send_file
 import getpass
-from datetime import datetime
-from datetime import timedelta
 import socket
+import json
+import os.path
 
 app = Flask(__name__)
 
-heartbeat_file_abspath = "/home/" + getpass.getuser() + "/heartbeat.txt"
+path_home = "/home/" + getpass.getuser() + "/"
+heartbeat_file_abspath = path_home + "heartbeat.txt"
+downtimes_file_abspath = path_home + "downtimes.json"
 hostname=socket.gethostname()
 IPAddr=socket.gethostbyname(hostname)
 def style():
@@ -26,34 +28,28 @@ def tr(input):
 def th(input):
     return "<th>" + input + "</th>"
 
-def downtime_table():
-    values = []
-    table = "<table><thead><th>downtime start</th><th>downtime end</th><th>duration (approx minutes)</th></thead>"
-    with open(heartbeat_file_abspath) as f_ping:
-        lines = f_ping.readlines()
-        for line in lines:
-            values.append(int("".join(c for c in line if c.isdigit())))
-
-    count = 0
-    for value in values:
-        if count > 0:
-            offset = value - values[count - 1]
-            if offset > 2:
-                date_before = datetime.strptime(values[count - 1].__str__(), '%Y%m%d%H%M')
-                date_after = datetime.strptime(value.__str__(), '%Y%m%d%H%M')
-                delta = (date_after - date_before) / timedelta(minutes=1)
-                if delta > 2:
-                    table += tr(th(date_before.__str__()) + th(date_after.__str__()) + th(delta.__str__()))
-        count+=1
-
-    table += "</table>"
-    return table
-
 def heading_text():
     return "<h1>Downtimes</h1>"
 
 def download_ping():
     return "<a href=http://" + IPAddr + ":8080/heartbeatfile download>download hearbeat file</a>"
+
+#
+# get Downtimes
+#
+def downtime_table():
+    table = "<table><thead><th>downtime start</th><th>downtime end</th><th>duration (approx minutes)</th></thead>"
+    if os.path.isfile(heartbeat_file_abspath) and os.path.isfile(downtimes_file_abspath):
+        with open(downtimes_file_abspath, "r+") as f:
+            downtimes = json.load(f)['downtimes']
+            for dowtime in downtimes:
+                table += tr(th(dowtime['shutdown']) + th(dowtime['startup']) + th(dowtime['delta']))
+    else:
+        table += tr(th("no data") + th("no data") + th("no data"))
+
+    return table
+
+
 @app.route('/')
 def hello():
     prolog = enclose(heading_text(), "div")
