bfc4b43658e8d5bbdcb34cbda0791498687a58b2
Chris <schauer@inf-schauer.de>
2023-12-02 17:52:37 +0100

    refactor

--- watcher_scripts/uninstall.sh
+++ watcher_scripts/uninstall.sh
@@ -6,4 +6,7 @@ sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
 sudo rm /etc/systemd/system/blackout_watch_startup.service
 sudo rm /etc/systemd/system/blackout_watch_server.service
 sudo systemctl daemon-reload
+# zur Sicherheit
+sudo systemctl unmask blackout_watch_startup.service
+sudo systemctl unmask blackout_watch_server.service
 
