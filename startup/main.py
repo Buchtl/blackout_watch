964e0ef8caebb6b623aca0d6a3e022833a2cddeb
Chris <schauer@inf-schauer.de>
2023-12-02 15:00:37 +0100

    refactor()

--- startup/main.py
+++ startup/main.py
@@ -8,26 +8,29 @@ from datetime import timedelta
 path_home = "/home/" + getpass.getuser() + "/"
 heartbeat_file_abspath = path_home + "heartbeat.txt"
 downtimes_json = path_home + "downtimes.json"
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
 
 # get last heartbeat as date
-def getLastHeartbeat():
+def get_last_heartbeat():
     with open(heartbeat_file_abspath) as f_ping:
         lines = f_ping.readlines()
         for line in lines:
             pass
-        last_heartbeat = int("".join(c for c in line if c.isdigit()))
-    return datetime.strptime(last_heartbeat.__str__(), '%Y%m%d%H%M')
+        last_heartbeat_str = int("".join(c for c in line if c.isdigit())).__str__()
+    return datetime.strptime(last_heartbeat_str.__str__(), '%Y%m%d%H%M')
+
 
 if os.path.isfile(heartbeat_file_abspath):
-    last_heartbeat = getLastHeartbeat()
+    last_heartbeat = get_last_heartbeat()
 else:
     exit(0)
 
 current = datetime.now()
 delta = (current - last_heartbeat) / timedelta(minutes=1)
-element = {'shutdown': last_heartbeat.__str__(), 'startup': current.strftime("%Y-%m-%d %H:%M:%S").__str__(), 'delta': delta.__str__()}
+element = {'shutdown': last_heartbeat.__str__(), 'startup': current.strftime("%Y-%m-%d %H:%M:%S").__str__(),
+           'delta': delta.__str__()}
 
 data = [element]
 if not os.path.isfile(downtimes_json):
