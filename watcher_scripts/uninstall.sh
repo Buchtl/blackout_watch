87212ad70c4cfebf4affe0e32c137e851eb7bdec
Chris <schauer@inf-schauer.de>
2023-12-02 17:53:38 +0100

    deactivated heartbeat in uninstall

--- watcher_scripts/uninstall.sh
+++ watcher_scripts/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo systemctl stop blackout_watch_heartbeat.service
+#sudo systemctl stop blackout_watch_heartbeat.service
 sudo systemctl stop blackout_watch_startup.service
 sudo systemctl stop blackout_watch_server.service
-sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
+#sudo rm /etc/systemd/system/blackout_watch_heartbeat.service
 sudo rm /etc/systemd/system/blackout_watch_startup.service
 sudo rm /etc/systemd/system/blackout_watch_server.service
 sudo systemctl daemon-reload
