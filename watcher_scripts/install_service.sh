f0f2e1803bfe3833c5c2e044e847a5ad9cdd0daf
Chris <schauer@inf-schauer.de>
2023-12-02 15:04:33 +0100

    changed order of start

--- watcher_scripts/install_service.sh
+++ watcher_scripts/install_service.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 sudo cp *.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable blackout_watch_heartbeat.service
+# Enable services
 sudo systemctl enable blackout_watch_startup.service
 sudo systemctl enable blackout_watch_server.service
-sudo systemctl start blackout_watch_heartbeat.service
+sudo systemctl enable blackout_watch_heartbeat.service
+# Start services
 sudo systemctl start blackout_watch_startup.service
 sudo systemctl start blackout_watch_server.service
+sudo systemctl start blackout_watch_heartbeat.service
