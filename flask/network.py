2e38280bf34d8bdd41e49ac9fb3e408d978763c5
Chris <schauer@inf-schauer.de>
2023-12-06 18:27:23 +0100

    detect local ip

--- flask/network.py
+++ flask/network.py
@@ -3,6 +3,10 @@ import logging
 import sys
 import re
 import time
+import subprocess
+
+
+# ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
 
 def get_logger():
     logger = logging.getLogger()
@@ -20,20 +24,23 @@ logger = get_logger()
 
 # Pattern = regex
 # delay: time between check for ip
-def get_ip(pattern: str, delay: int):
-    result = ""
-    while not re.search(pattern, result):
-        for interface in ni.interfaces():
-            addr = str(ni.ifaddresses(interface)[ni.AF_INET][0]['addr'])
-            if re.search(pattern, addr):
-                result = addr
-                logger.debug("Pattern " + pattern + " matches " + addr)
-        if not result:
-            logger.debug("No matching interface yet. Waiting " + delay.__str__() + " seconds for next approach.")
+def get_ip(ip_pattern: str, delay: int):
+    cmd = ("ip -4 -br a | egrep '%s' | awk  '{print $3}' | awk -F '/' '{print $1}'" % (ip_pattern))
+
+    ip = ""
+    while not re.search(ip_pattern, ip):
+        ips = str(subprocess.check_output(cmd, shell=True, universal_newlines=True)).strip().split("\n")
+        detected_ip = str
+        for ip in ips:
+            if re.search(ip_pattern, ip):
+                detected_ip = ip
+                break
+
+        if not detected_ip:
             time.sleep(delay)
+    return ip
 
-    return result
 
-
-pattern = "192.{3}[0-9].[0-9]{1,3}"
-logger.debug(" ip is " + get_ip(pattern, 5))
+if __name__ == "__main__":
+    pattern = "192.{3}[0-9].[0-9]{1,3}.[0-9]{1,3}"
+    logger.debug(" ip is " + get_ip(pattern, 5))
