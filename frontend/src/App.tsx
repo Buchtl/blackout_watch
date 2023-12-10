b28e5871061b310315c1d38275ea7bc525c2b629
Chris <schauer@inf-schauer.de>
2023-12-10 15:28:49 +0100

    download via redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`http://${process.env.REACT_APP_BACKEND_URL}/heartbeatfile`}
         >
-          Learn React
+          Download heartbeat
         </a>
       </header>
     </div>
